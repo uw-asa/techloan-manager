019feaa842ba85da17df49d1286ac45934f826d6
Bradley Bell <bradleyb@uw.edu>
2016-10-28 20:13:41 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="techloan-manager",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'Django',
+    ],
+)
